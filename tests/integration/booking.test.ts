ea378e36ae4eef0ce471dc07faafb9b9cf01e152
Sergio <sergiomendes23@gmail.com>
2022-12-04 13:20:42 -0300

    test when no roomId exists done

--- tests/integration/booking.test.ts
+++ tests/integration/booking.test.ts
@@ -3,13 +3,7 @@ import faker from "@faker-js/faker";
 import httpStatus from "http-status";
 import * as jwt from "jsonwebtoken";
 import supertest from "supertest";
-import {
-  createUser,
-  createHotel,
-  createRoomWithHotelId,
-  createBookingData,
-  createTicketTypeRemote,
-} from "../factories";
+import { createUser, createHotel, createRoomWithHotelId, createBookingData } from "../factories";
 
 import { cleanDb, generateValidToken } from "../helpers";
 
@@ -50,9 +44,9 @@ describe("GET /booking", () => {
   describe("when token is valid", () => {
     it("should respond with status 404 when user is not logged in", async () => {
       const token = await generateValidToken();
-      
+
       const response = await server.get("/booking").set("Authorization", `Bearer ${token}`);
-      
+
       expect(response.status).toBe(httpStatus.NOT_FOUND);
     });
     it("should respond with status 200 when booking is included in rooms data", async () => {
@@ -76,6 +70,45 @@ describe("GET /booking", () => {
           updatedAt: room.updatedAt.toISOString(),
         },
       });
+    });
+  });
+});
+
+describe("POST /booking", () => {
+  it("should respond with status 401 if no token is given", async () => {
+    const response = await server.get("/booking");
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if given token is not valid", async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.get("/booking").set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if there is no session for given token", async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.get("/booking").set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe("when token is valid", () => {
+    it("should respond with status 404 when no roomId exists", async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const hotel = await createHotel();
+      await createRoomWithHotelId(hotel.id);
+      const body = { roomId: "" };
+
+      const response = await server.get("/booking").set("Authorization", `Bearer ${token}`).send(body);
+
+      expect(response.status).toBe(httpStatus.NOT_FOUND);
     });
   });
 });
